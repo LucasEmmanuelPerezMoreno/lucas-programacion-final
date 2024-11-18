09d3dde07bdbf76059a7cded895e3aed115fa564
LucasEmmanuelPerezMoreno <lucasperez.06@hotmail.com>
2024-11-18 16:53:13 -0300

    fundamentos

---
+++ CLASE-16/FUNDAMENTOS Y TIPOS DE DATOS/clase-16.js
@@ -0,0 +1,38 @@
+//comenatarios
+
+//Esto me va a permitir colocar un comentario
+
+
+/*Esto crea un comentario multi linea*/
+
+
+//variables
+let anio = 2024;
+
+//camelcase (primera letra con minuncula y cuando cambia la palabra inicia con mayuscula)
+let anioDeNacimiento = 1988
+
+//OPERACIONES BASICAS CON NUMEROS
+const PI = 3.14;
+
+let valor = 5 + 7
+let numero1 = 10
+let numero2 = 20
+let suma = numero1 + numero2
+let resta = numero1 - numero2
+let dividir = numero1 / numero2
+  
+
+//strings
+//concatenaascion
+let texto = 'Hola mundo'
+let texto2 = 'JavaScript'
+let resultado = texto + texto2
+
+//OPERACIONES COMPARATIVOS (BOOLEANOS)
+let mayor = 5 >10 //me va a devolver falso
+let comparar = 5 == 5 //true
+
+
+
+
